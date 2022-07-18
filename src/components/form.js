8e0d2c9df32273b157d6cc6e6c0c41f7b2c3cc07
Vineetsingh07 <vineetsingh2603@gmail.com>
2022-07-19 00:29:03 +0530

    First Commit.

---
+++ src/components/form.js
@@ -0,0 +1,51 @@
+import * as React from "react";
+import { Grid } from "@mui/material";
+
+export default function Form({ addChild }) {
+  const [initialData, setInitialData] = React.useState({ name: "", age: "" });
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+
+    const value = e.target.value;
+
+    setInitialData({ ...initialData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setInitialData(initialData);
+    addChild(initialData);
+    setInitialData({ name: "", age: "" });
+  };
+
+  return (
+    <Grid container spacing={0} align="center">
+      <form onSubmit={handleSubmit}>
+        <Grid item xs={6}>
+          <label>
+            Name:
+            <input
+              type="text"
+              name="name"
+              value={initialData.name}
+              onChange={handleChange}
+            />
+          </label>
+        </Grid>
+        <Grid item xs={6}>
+          <label>
+            Age:
+            <input
+              type="number"
+              name="age"
+              value={initialData.age}
+              onChange={handleChange}
+            />
+          </label>
+        </Grid>
+        <button>Submit</button>
+      </form>
+    </Grid>
+  );
+}
---
+++ src/components/parents.js
@@ -0,0 +1,121 @@
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Form from "./form";
+import Button from "@mui/material/Button";
+
+function Parents() {
+  const [parentData, setParentData] = React.useState([
+    {
+      name: "Abc",
+      age: 21,
+      children: [
+        { name: "C", age: 10, price: 10 },
+        { name: "D", age: 12, price: 20 },
+        { name: "E", age: 15, price: 30 },
+      ],
+    },
+    {
+      name: "xyz",
+      age: 25,
+      children: [
+        { name: "F", age: 20, price: 110 },
+        { name: "G", age: 22, price: 120 },
+        { name: "H", age: 35, price: 130 },
+      ],
+    },
+  ]);
+
+  const [selectedParent, setSelectedParent] = React.useState();
+
+  const [sum, setSum] = React.useState(-1);
+
+  const [showForm, setShowForm] = React.useState(false);
+
+  const sumOfPrice = () => {
+    let _sum = 0;
+    for (let index = 0; index < selectedParent.children.length; index++) {
+      const element = selectedParent.children[index];
+      _sum += Number(element.age);
+    }
+    setSum(_sum);
+  };
+
+  const addChild = (obj) => {
+    console.log("obj", obj);
+
+    const newObject = { ...selectedParent };
+    newObject.children.push(obj);
+
+    setSelectedParent(newObject);
+  };
+
+  return (
+    <>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                <strong>Parent: {selectedParent && selectedParent.name}</strong>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell align="right">Name</TableCell>
+              <TableCell align="right">Age</TableCell>
+            </TableRow>
+            {parentData.map((parent) => (
+              <TableRow key={parent} onClick={() => setSelectedParent(parent)}>
+                <TableCell align="right">{parent.name}</TableCell>
+                <TableCell align="right">{parent.age}</TableCell>
+              </TableRow>
+            ))}
+            <TableRow>
+              <TableCell>Children</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell align="right">Name</TableCell>
+              <TableCell align="right">Age</TableCell>
+            </TableRow>
+            {selectedParent &&
+              selectedParent.children.map((child) => (
+                <TableRow key={child}>
+                  <TableCell align="right">{child.name}</TableCell>
+                  <TableCell align="right">{child.age}</TableCell>
+                </TableRow>
+              ))}
+
+            <TableRow>
+              <TableCell align="right">
+                <Button variant="contained" onClick={() => setShowForm(true)}>
+                  Add Child
+                </Button>
+              </TableCell>
+              <TableCell align="right">
+                <Button variant="contained" onClick={() => sumOfPrice()}>
+                  Price
+                </Button>
+              </TableCell>
+            </TableRow>
+            {sum !== -1 && (
+              <TableRow>
+                <TableCell align="right">Price</TableCell>
+                <TableCell align="right">{sum}</TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+        {showForm && <Form addChild={addChild} />}
+      </TableContainer>
+    </>
+  );
+}
+
+export default Parents;
